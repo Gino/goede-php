96a71e1c05366453a62ac27d755399fbe97e0195
Gino <ginolouwsma01@gmail.com>
2018-01-24 10:05:46 +0100

    Added sun, plane and ufo flying in the sky.

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
-    date_default_timezone_set("Europe/Amsterdam");
+    // date_default_timezone_set('Australia/Eucla'); // Middag
+    // date_default_timezone_set('America/Aruba'); // Nacht/avond
+    date_default_timezone_set('Europe/Amsterdam'); // Ochtend
 
-    $date = date("H");
-    $time = date("H:i");
-    if ($date >= "06" && $date < "12") {
-        $status = "morning";
-        $greeting = "Goedemorgen";
-    } elseif ($date >= "12" && $date < "18") {
-        $status = "afternoon";
-        $greeting = "Goedemiddag";
-    } elseif ($date >= "18" && $date < "00") {
-        $status = "evening";
-        $greeting = "Goedeavond";
-    } elseif ($date >= "00" && $date < "06") {
-        $status = "night";
-        $greeting = "Goedenacht";
+    $date = date('H');
+    $time = date('H:i');
+    if ($date >= '06' && $date < '12') {
+        $status = 'morning';
+        $greeting = 'Goedemorgen';
+        $picture = 'zonnetje.png';
+    } elseif ($date >= '12' && $date < '18') {
+        $status = 'afternoon';
+        $greeting = 'Goedemiddag';
+        $picture = 'vliegtuig.png';
+    } elseif ($date >= '18' && $date < '00') {
+        $status = 'evening';
+        $greeting = 'Goedeavond';
+        $picture = 'ufo.png';
+    } elseif ($date >= '00' && $date < '06') {
+        $status = 'night';
+        $greeting = 'Goedenacht';
+        $picture = 'ufo.png';
     }
 ?>
 <!DOCTYPE html>
@@ -28,6 +34,11 @@
 <style>
     @import url('https://fonts.googleapis.com/css?family=Lobster');
 
+    body {
+        overflow-x: hidden;
+
+    }
+
     div {
         font-family: 'Lobster', cursive;
         color: white;
@@ -40,8 +51,22 @@
     p {
         margin: 0;
     }
+
+    img {
+        position: absolute;
+        animation-name: flying;
+        animation-duration: 10s;
+        animation-iteration-count: infinite;
+    }
+
+    @keyframes flying {
+        0%   {left: -20%;top: 0%}
+        100%   {left: 100%;top: 10%}
+        150%   {left: 150%;top: 10%}
+    }
 </style>
 <body style="background: url('backgrounds/<?php echo $status; ?>.png');">
+    <img src="img/<?php echo $picture; ?>" alt="Flying picture">
     <div>
         <p><?php echo $greeting; ?>, Gino</p>
         <p>Het is nu <?php echo $time; ?></p>
